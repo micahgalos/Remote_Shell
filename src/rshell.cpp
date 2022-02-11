7cb1f418ee05aa93a9bbe2fdc26f260b0fbe782d
micahgalos <micahzugalos@gmail.com>
2022-02-10 20:09:03 -0800

    split rshell .cpp & .h

--- src/rshell.cpp
+++ src/rshell.cpp
@@ -22,72 +22,6 @@
 
 using namespace std;
 
-//starting screen of the command, the first line
-void rShell::show() { cout << "$ "; }
-
-//this is to clears all the vectors in the process of doing the parsing for the command and connectors that are  going through the command to give the answer
-void rShell::clear() { 
-    listUser.clear();
-    vecdex.clear();
-    connectingList.clear();
-        if (!parenthesis.empty()){	
-        stack<string>().swap(parenthesis);
-    }
-} 
-//lets say see echo a;(echo b; echo c), we dont need parenthesis so this functions deletes those parenthesis
-void rShell::parenthesisParsing() { 
-    for (unsigned int g = 0; g < listUser.size(); ++g) {
-        if (listUser[g] == "(" || listUser[g] == ")") {
-            listUser.erase(listUser.begin() + g);
-            --g;
-        }
-    }
-}
-
-//ignores commands that we may see that have a # in front of them because they are not necessary
-void rShell::commentParsing() {
-    for (unsigned int g = 0; g < userInput.size(); ++g) {
-        if (userInput[g] == '#') {
-            userInput.erase(g, userInput.size() - 1);
-            break;
-        }
-    }
-}
-//after the userSpacing, if there are two things in the command that are indexices then it makes sure that they are split 
-void rShell::userSplitting() {
-    istringstream istream(userInput);
-    string tokening;
-    while(istream >> tokening) { listUser.push_back(tokening); }
-}
-//this is a function that takens in the spacing between the command for example if we had echo  a; seperates each command argument into     one index
-//this had similarities to the C langauage it made the most sense to me, after thinking about it for awhile
-// it goes back to when I used it in Embedded Systems
-void rShell::userSpacing() {  	
-    for (unsigned int g = 0; g < userInput.size(); ++g) {
-        switch (userInput[g]) {
-			case ';':  //these are cases and I was looking at for the spacing 
-			if (g > 0) { (userInput[g - 1] != ' ') ? userInput.insert(g, " ") : userInput; }
-			(userInput[g + 1] != ' ') ? userInput.insert(g + 1, " ") : userInput;
-			++g;
-			break;
-            
-			case '(':  //another case where you focus on a parenthesis
-            if (g > 0) { (userInput[g - 1] != ' ') ? userInput.insert(g, " ") : userInput; }
-            (userInput[g + 1] != ' ') ? userInput.insert(g + 1, " ") : userInput;
-            ++g;
-            break;
-
-            case ')':
-            if (g > 0) { (userInput[g - 1] != ' ') ? userInput.insert(g, " ") : userInput; }
-            (userInput[g + 1] != ' ') ? userInput.insert(g + 1, " ") : userInput;
-            ++g;
-			break;
-            
-			default:     //when its not specifed this case automatically will break meaning end 
-            break;
-        }
-    }
-}
 //temp is the for the recusive calls and connectingList is between parenthesis when given a command
 void rShell::connectParsing(unsigned int temp, vector<connectingStrings*> & connectingList) {    	
 	connectingStrings* connectors;
@@ -185,60 +119,6 @@ void rShell::connectParsing(unsigned int temp, vector<connectingStrings*> & conn
 		}
 		g = temp;
 	}    
-}
-
-// ( to ) including multiple ones
-void rShell::parsing(string commandline) {
-    unsigned int temp = 0;
-    userInput = commandline;
-    clear();
-    commentParsing();
-    userSpacing();
-    userSplitting();
-    connectParsing(temp, connectingList);
-    parenthesisParsing();	
-}
-
-//this is simple case of echo a; echo b where you check for one command and you parse another accordingly
-//commandlist has all the commands due to the pushes  	
-void rShell::constructCommands() {
-	cmd = new Command(); //this is an inializer 
-	bool next = true;
-	for (unsigned int g = 0; g < listUser.size(); ++g) {
-		bool b1 = listUser[g] == ";" || listUser[g] == "||";
-		bool b2 = listUser[g] == "&&" || listUser[g] == "<";
-		bool b3 = listUser[g] == ">" || listUser[g] == ">>";
-		bool b4 = b1 || b2;
-		bool b5 = b3 || listUser[g] == "|";
-		bool b6 = b4 || b5;
-		if (b6){			
-			commandlist.push(cmd);
-			//we do it again for the second time
-			if ((g + 1) < listUser.size()) { cmd = new Command(); }
-			else { next = false; }
-		}
-		//still need to create this in base file 
-		else { cmd->comm.push_back(listUser[g]); }
-	}
-	if (next) { commandlist.push(cmd); }
-}
-
-//checks for at least two commands with a connect so that we can build a tree to get the answer for the user command
-void rShell::commandDesign() {
-	constructCommands();
-	if (commandlist.size() >= 2) {
-        treeDesign.push_back(designtree(connectingList));
-        while (!treeDesign.empty()){
-                treeDesign.back()->construct();
-                treeDesign.pop_back();
-        }
-    }
-    else {
-        while (!commandlist.empty()) {
-                commandlist.front()->construct();
-                commandlist.pop();
-        }
-    }
 }
 
 Base* rShell::designtree(vector<connectingStrings*> connectingList) {
@@ -360,7 +240,9 @@ Base* rShell::designtree(vector<connectingStrings*> connectingList) {
 			}
 		}
 
-		else if (connectingList[g]->num == "<"){ //follows the same format as above but with input                                                 //redirection connector
+		else if (connectingList[g]->num == "<"){ 
+		// follows the same format as above but with input
+		// redirection connector
 			if (treeDesign.empty()) {
 				if (connectingList[g]->templist.empty()) {
 					leftside = commandlist.front();
@@ -397,7 +279,9 @@ Base* rShell::designtree(vector<connectingStrings*> connectingList) {
 			}
 		}
 
-	    else if (connectingList[g]->num == ">"){ //follows the same format as above but with output                                                //redirection connector
+		else if (connectingList[g]->num == ">"){ 
+		//follows the same format as above but with output                                                
+		//redirection connector
 			if (treeDesign.empty()) {
 				if (connectingList[g]->templist.empty()) {
 					leftside = commandlist.front();
@@ -434,7 +318,9 @@ Base* rShell::designtree(vector<connectingStrings*> connectingList) {
 			}
 		}
 
-		else if (connectingList[g]->num == ">>"){ //follows the same format as above but with                                                      // double output redirection connector
+		else if (connectingList[g]->num == ">>"){ 
+		//follows the same format as above but with
+		// double output redirection connector
 			if (treeDesign.empty()) {
 				if (connectingList[g]->templist.empty()) {
 					leftside = commandlist.front();
